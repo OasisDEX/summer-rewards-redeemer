7699a4cf596beb05ef1533874755e1a77d888823
halaprix <panicz.klos@gmail.com>
2023-08-28 14:28:10 +0200

    chore: add test test for custom reward ratio

--- packages/common/utils/test.utils.ts
+++ packages/common/utils/test.utils.ts
@@ -4,7 +4,7 @@ import { graphClient } from "./graph.utils";
 
 export const graphStub = sinon.stub(graphClient, "WeeklyRewards");
 
-export function setupGraphStub() {
-  graphStub.onCall(0).resolves(weeklyRewardData);
+export function setupGraphStub(weeklyRewards:any = weeklyRewardData) {
+  graphStub.onCall(0).resolves(weeklyRewards);
  
 }
--- packages/snapshot/get-snapshot.test.ts
+++ packages/snapshot/get-snapshot.test.ts
@@ -1,19 +1,23 @@
 import { describe, expect, jest, test } from "@jest/globals";
-import { getWeeklySnapshot } from "./get-snapshot";
+import { getDailySnapshot, getWeeklySnapshot } from "./get-snapshot";
 import { graphStub, setupGraphStub } from "common/utils/test.utils";
-import { weeklyRewardSnapshot as expectedSnapshot } from "common/utils/data";
+import { dailyRewardsDataWithCustomRatios, weeklyRewardSnapshot as expectedWeeklySnapshot, dailyRewardsSnapshotWithCustomRatios as expectedDailySnapshot } from "common/utils/data";
 import sinon from "sinon";
 
 describe("Process snapshot", () => {
-  beforeEach(async () => {
-    setupGraphStub();
-  });
   afterEach(async () => {
     sinon.restore();
   });
   test("should verify the saved snapshot against calculation execution", async () => {
+    setupGraphStub();
     const parsedSnapshot = await getWeeklySnapshot(1);
-    expect(parsedSnapshot).toEqual(expectedSnapshot);
+    expect(parsedSnapshot).toEqual(expectedWeeklySnapshot);
+    sinon.assert.calledOnce(graphStub);
+  });
+  test("should verify the saved snapshot against calculation execution - custom earn/borrow ratio", async () => {
+    setupGraphStub(dailyRewardsDataWithCustomRatios);
+    const parsedSnapshot = await getDailySnapshot(19587);
+    expect(parsedSnapshot).toEqual(expectedDailySnapshot);
     sinon.assert.calledOnce(graphStub);
   });
   jest.setTimeout(100000); // 100 seconds
