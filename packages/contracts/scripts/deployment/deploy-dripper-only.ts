ead772966dc976a2d06189946085696df172a2e5
Roberto Cano <3525807+robercano@users.noreply.github.com>
2023-10-20 12:06:15 +0200

    chore: deploy Ajna dripper to mainnet

--- packages/contracts/scripts/deployment/deploy-dripper-only.ts
+++ packages/contracts/scripts/deployment/deploy-dripper-only.ts
@@ -1,7 +1,7 @@
 import chalk from "chalk";
 import hre, { network } from "hardhat";
 
-import { AjnaDripper, AjnaRedeemer } from "typechain-types";
+import { AjnaDripper } from "typechain-types";
 import { config } from "common/config";
 import { getOrDeployContract } from "../utils/hardhat.utils";
 
@@ -10,22 +10,13 @@ async function main() {
     config.addresses.ajnaToken,
     config.addresses.admin,
   ]);
-  const ajnaRedeemer = await getOrDeployContract<AjnaRedeemer>("AjnaRedeemer", [
-    config.addresses.ajnaToken,
-    config.addresses.operator,
-    ajnaDripper.address,
-  ]);
-  console.log(`Redeemer deployed to : ${chalk.green(ajnaRedeemer.address)} on ${chalk.green(config.network)}`);
   console.log(`Dripper deployed to : ${chalk.green(ajnaDripper.address)} on ${chalk.green(config.network)}`);
+
   if (network.name === "mainnet" || network.name === "goerli") {
     // Add a 10 second wait
     console.log(`Waiting for 60 seconds...`);
     await new Promise((resolve) => setTimeout(resolve, 60000));
 
-    await hre.run("verify:verify", {
-      address: ajnaRedeemer.address,
-      constructorArguments: [config.addresses.ajnaToken, config.addresses.operator, ajnaDripper.address],
-    });
     await hre.run("verify:verify", {
       address: ajnaDripper.address,
       constructorArguments: [config.addresses.ajnaToken, config.addresses.admin],
